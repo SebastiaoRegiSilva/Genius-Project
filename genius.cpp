60f5ae7d7396716efe81a9cca75a12f86f634671
Sebastião Silva <swillian2009@gmail.com>
2022-04-13 22:15:34 -0300

    Reserva.

--- genius.cpp
+++ genius.cpp
@@ -16,10 +16,10 @@ void setup()
     pinMode(pin2, OUTPUT); // Buzzer 
     pinMode(pin3, OUTPUT); // LED Vermelho.
     pinMode(pin4, OUTPUT); // LED Amarelo.
-    pinMode(pin5, OUTPUT); // LED Verde
-    pinMode(pin11, INPUT);
-    pinMode(pin12, INPUT);
-    pinMode(pin13, INPUT);
+    pinMode(pin5, OUTPUT); // LED Verde.
+    pinMode(pin11, INPUT); // Botão do Vermelho.
+    pinMode(pin12, INPUT); // Botão do Amarelo.
+    pinMode(pin13, INPUT); // Botão do Verde.
 }
 
 void loop()
@@ -51,10 +51,11 @@ void loop()
             }
         break;
             
+        // Fase 1 do jogo vai acender o verde.
         case 1:
             digitalWrite(pin5, HIGH); // Verde.
-            digitalWrite(pin4, LOW); // Amarelo.
-            digitalWrite(pin3, LOW); // Vermelho.
+            digitalWrite(pin4, LOW);
+            digitalWrite(pin3, LOW); 
             delay(500);
             digitalWrite(pin5, LOW);
             digitalWrite(pin4, LOW);
@@ -79,25 +80,23 @@ void loop()
             if (erro == 1)
                 estado = 11;
             else
-            {
                 estado = 2;
-                delay(500); // Segurar o LED acesso. 
-            }
             
         break;
 
+        // Fase 2 do jogo vai acender o Verde e o Vermelho.
         case 2:
-            digitalWrite(pin5, LOW); 
+            digitalWrite(pin5, HIGH); // LED Verde.
             digitalWrite(pin4, LOW); 
-            digitalWrite(pin3, HIGH); // LED Vermelho.
+            digitalWrite(pin3, LOW); 
             delay(500);
             digitalWrite(pin5, LOW);
             digitalWrite(pin4, LOW);
             digitalWrite(pin3, LOW);
             delay(500);
-            digitalWrite(pin5, HIGH); // Led Verde.
+            digitalWrite(pin5, LOW); 
             digitalWrite(pin4, LOW);
-            digitalWrite(pin3, LOW);
+            digitalWrite(pin3, HIGH); // LED Vermelho.
             delay(500);
             digitalWrite(pin5, LOW);
             digitalWrite(pin4, LOW);
@@ -117,7 +116,7 @@ void loop()
                     break;
                 
                 x1 = digitalRead(pin13); // Verde.
-                x2 = digitalRead(pin12); // Amarelo.
+				x2 = digitalRead(pin12); // Amarelo.
                 x3 = digitalRead(pin11); // Vermelho.
 
                 // Implemento a avaliação de qual botão foi apertado.
@@ -126,9 +125,9 @@ void loop()
                     // Espero o primeiro botão da sequência.
                     case 0: 
                         // Apertei o botão certo e sigo na avaliação da fase.
-                        if (x3 == 1)
+                        if (x1 == 1)
                             sequencia = 1; 
-                        else if (x2 == 1 || x1 == 1)
+                        else if (x2 == 1 || x3 == 1)
                         {
                             // Errei o botão, seto a flag de erro e fim para sair do while.
                             erro = 1;
@@ -138,7 +137,7 @@ void loop()
 
                     case 1:
                         // Apertei o botão certo e encerro o while sem a flag de erro.
-                        if (x1 == 1)
+                        if (x3 == 1)
                             fim = 1; 
                         // Errei o botão, seto a flag de erro e fim para sair do while.
                         else if (x1 == 1 || x2 == 1)
@@ -158,91 +157,89 @@ void loop()
             
             break;
 
+        // Fase 3 do jogo vai acender o Verde, Vermelho e Amarelo.
         case 3:
-            digitalWrite(6, LOW);
-            digitalWrite(3, LOW);
-            digitalWrite(10, HIGH);
+            digitalWrite(pin5, HIGH); // Verde. 
+            digitalWrite(pin4, LOW);
+            digitalWrite(pin3, LOW); 
             delay(500);
-            digitalWrite(6, LOW);
-            digitalWrite(3, LOW);
-            digitalWrite(10, LOW);
+            digitalWrite(pin5, LOW);
+            digitalWrite(pin4, LOW);
+            digitalWrite(pin3, LOW);
             delay(500);
-            digitalWrite(6, HIGH);
-            digitalWrite(3, LOW);
-            digitalWrite(10, LOW);
+            digitalWrite(pin5, LOW); 
+            digitalWrite(pin4, LOW);
+            digitalWrite(pin3, HIGH); // Vermelho.
             delay(500);
-            digitalWrite(6, LOW);
-            digitalWrite(3, LOW);
-            digitalWrite(10, LOW);
+            digitalWrite(pin5, LOW);
+            digitalWrite(pin4, LOW);
+            digitalWrite(pin3, LOW);
             delay(500);
-            digitalWrite(6, LOW);
-            digitalWrite(3, HIGH);
-            digitalWrite(10, LOW);
+            digitalWrite(pin5, LOW); 
+            digitalWrite(pin4, HIGH); // Amarelo.
+            digitalWrite(pin3, LOW);
             delay(500);
-            digitalWrite(6, LOW);
-            digitalWrite(3, LOW);
-            digitalWrite(10, LOW);
+            digitalWrite(pin5, LOW);
+            digitalWrite(pin4, LOW);
+            digitalWrite(pin3, LOW);
 
-            sequencia = 0; // Criei uma variável pra armazenar qual ponto da sequencia de botões o jogador está
+            sequencia = 0;
             erro = 0;
-            fim = 0; //variavel pra avaliar se devo encerrar o loop de verificação dos botões
+            fim = 0;
+            
+            // Loop infinito
             while (1)
             {
                 if (fim == 1)
-                { //se fim é um, saio do while e sigo a execução da máquina de estados
                     break;
-                }
-                x1 = digitalRead(13);
-                x2 = digitalRead(12);
-                x3 = digitalRead(11);
+                
+                x1 = digitalRead(pin13);
+                x2 = digitalRead(pin12);
+                x3 = digitalRead(pin11);
 
                 switch (sequencia)
-                {       // Implemento a avaliação de qual botão foi apertado
-                case 0: //espero o primeiro botão da sequência
-                    if (x1 == 1)
-                    {
-                        sequencia = 1; //Apertei o botão certo e sigo na avaliação da fase
-                    }
-                    else if (x2 == 1 || x3 == 1)
-                    {
-                        erro = 1;
-                        fim = 1; //Errei o botão, seto a flag de erro e fim para sair do while
-                    }
+                {       
+                    case 0:
+                        // Pressiona o botão que liga o verde.
+                        if (x1 == 1)
+                            sequencia = 1;
+                        else if (x2 == 1 || x3 == 1)
+                        {
+                            erro = 1;
+                            fim = 1; 
+                        }
                     break;
 
-                case 1:
-                    if (x2 == 1)
-                    {
-                        sequencia = 2; //Apertei o botão certo e sigo na avaliação da fase
-                    }
-                    else if (x1 == 1 || x3 == 1)
-                    {
-                        erro = 1;
-                        fim = 1; //Errei o botão, seto a flag de erro e fim para sair do while
-                    }
+                    case 1:
+                        // Pressiona o botão que liga o vermelho.
+                        if (x3 == 1)
+                            sequencia = 2;
+                        else if (x1 == 1 || x2 == 1)
+                        {
+                            erro = 1;
+                            fim = 1; 
+                        }
                     break;
 
-                case 2:
-                    if (x3 == 1)
-                    {
-                        fim = 1; //Apertei o botão certo e encerro o while sem a flag de erro
-                    }
-                    else if (x1 == 1 || x2 == 1)
-                    {
-                        erro = 1;
-                        fim = 1; //Errei o botão, seto a flag de erro e fim para sair do while
-                    }
+                    case 2:
+                        // Pressiona o botão que liga o amarelo.
+                        if (x2 == 1)
+                            fim = 1;
+                        else if (x1 == 1 || x3 == 1)
+                        {
+                            erro = 1;
+                            fim = 1; 
+                        }
                     break;
                 }
             }
+            
+            // Verificação se há erros.
             if (erro == 1)
-            {
                 estado = 11;
-            }
             else
-            {
                 estado = 4;
-            }
-            break;
-        }
+            
+        break;
+    }
 }
