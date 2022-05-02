7b05437639760ace397cd8dfe03904c63b959017
Sebastião Willian Pereira Regi <84387616+SebastiaoRegiSilva@users.noreply.github.com>
2022-05-02 19:23:41 -0300

    Correção em comentários.

--- contador.cpp
+++ contador.cpp
@@ -1,34 +1,33 @@
-// Define os valores dos digitos
-const byte digitos[10][7] = {{1,1,1,1,1,1,0},
-					   		 {0,1,1,0,0,0,0},
-					   		 {1,1,0,1,1,0,1},
-					   		 {1,1,1,1,0,0,1},
-					   		 {0,1,1,0,0,1,1},
-  					   		 {1,0,1,1,0,1,1},
-					   		 {1,0,1,1,1,1,1},
-					   		 {1,1,1,0,0,0,0},
-				       		 {1,1,1,1,1,1,1},
-					   		 {1,1,1,0,0,1,1}};
+/*
+	Define os valores dos dígitos.
+    Cada dígito representa um traço dentro do segmento, 
+    sendo 0 (apagado) e 1 (aceso).
+*/
+const byte digitos[10][7] = 
+{
+  	{1,1,1,1,1,1,0},
+	{0,1,1,0,0,0,0},
+	{1,1,0,1,1,0,1},
+	{1,1,1,1,0,0,1},
+	{0,1,1,0,0,1,1},
+  	{1,0,1,1,0,1,1},
+	{1,0,1,1,1,1,1},
+	{1,1,1,0,0,0,0},
+	{1,1,1,1,1,1,1},
+	{1,1,1,0,0,1,1}
+};
 
 void setup()
 {
   for(int i = 13; i >= 7; i--)
-  {
     pinMode(i,OUTPUT);
-  }
 }
 
 void loop(){
-  for(int digito = 0; digito < 10; digito++){ // Percorre os digitos
-    for(int led = 0; led < 7; led++){ // Percorre os valores dos leds
-    	digitalWrite((13-led),digitos[digito][led]);// Define valor lógico do led
-    }
+  for(int digito = 0; digito < 10; digito++)
+  {
+    for(int led = 0; led < 7; led++)
+    	digitalWrite((13-led),digitos[digito][led]);    
     delay(1000);
   }
 }
-
-/*
-OBS: Se a escolha for display catodo comum, ligue o resistor no
-GND, se for anodo comun ligue o resistor no 5V e altere a matriz
-ou inverta os valoresdos led com o operador de negacao "!".
-*/
